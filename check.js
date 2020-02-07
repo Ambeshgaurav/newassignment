7c8afa7359a2958c22726d96d5c61cbae1fa83b1
Ambesh Singh <ambesh.singh@deltaschool.in>
2020-02-07 16:19:58 +0530

    modification done

---
+++ check.js
@@ -0,0 +1,134 @@
+
+var b={
+    "_id" : ObjectId("5e3437d4e2d1113a3d47cf99"),
+    "user_id" : ObjectId("5e24a213e05aebb919be6376"),
+    "user_email" : "test1@test.com",
+    "form_data" : {
+        "form_name" : "testing@29",
+        "form_desc" : "testing@29",
+        "form_background" : "#c0bebe",
+        "project_name" : "testing@29",
+        "form_controls" : [
+            {
+                "section_description" : "",
+                "section_form_title" : "",
+                "_id" : "5e31bdf0022f55171c29e536",
+                "section_name" : "Default section",
+                "section_controls" : [
+                    {
+                        "class_name" : null,
+                        "description" : "Enter your name",
+                        "error_text" : null,
+                        "icon" : "fa-font",
+                        "input_color" : null,
+                        "label_color" : null,
+                        "label" : "Text",
+                        "name" : "text-1580318156777",
+                        "placeholder" : "Enter your name",
+                        "regex" : "",
+                        "subtype" : "text",
+                        "min" : 0,
+                        "max" : 0,
+                        "required" : false,
+                        "toggle" : false,
+                        "_id" : "5e31bdf0022f55171c29e537",
+                        "type" : "text",
+                        "handle" : true
+                    },
+                    {
+                        "class_name" : null,
+                        "description" : "Enter your email",
+                        "error_text" : null,
+                        "icon" : "fa-envelope",
+                        "input_color" : null,
+                        "label_color" : null,
+                        "label" : "Email",
+                        "name" : "email-1580318160067",
+                        "placeholder" : "Enter your email",
+                        "regex" : "^([a-zA-Z0-9_.-]+)@([a-zA-Z0-9_.-]+).([a-zA-Z]{2,5})$",
+                        "subtype" : "text",
+                        "min" : 0,
+                        "max" : 0,
+                        "required" : true,
+                        "toggle" : false,
+                        "_id" : "5e31bdf0022f55171c29e538",
+                        "type" : "email",
+                        "handle" : true
+                    },
+                    {
+                        "class_name" : null,
+                        "description" : "Enter your phone",
+                        "error_text" : null,
+                        "icon" : "fa-phone",
+                        "input_color" : null,
+                        "label_color" : null,
+                        "label" : "Phone",
+                        "name" : "phone-1580318163582",
+                        "placeholder" : "Enter your phone",
+                        "regex" : "^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$",
+                        "subtype" : "text",
+                        "min" : 0,
+                        "max" : 0,
+                        "required" : false,
+                        "toggle" : false,
+                        "_id" : "5e31bdf0022f55171c29e539",
+                        "type" : "phone",
+                        "handle" : true
+                    }
+                ]
+            },
+            {
+                "section_description" : "",
+                "section_form_title" : "",
+                "_id" : "5e31bdf0022f55171c29e53a",
+                "section_name" : "section1",
+                "section_controls" : [
+                    {
+                        "class_name" : null,
+                        "description" : null,
+                        "error_text" : null,
+                        "icon" : "fa-calendar",
+                        "input_color" : null,
+                        "label_color" : null,
+                        "label" : "Date",
+                        "name" : "date-1580318177143",
+                        "placeholder" : "Date",
+                        "regex" : null,
+                        "subtype" : null,
+                        "min" : 0,
+                        "max" : 0,
+                        "required" : false,
+                        "toggle" : false,
+                        "_id" : "5e31bdf0022f55171c29e53b",
+                        "type" : "date"
+                    },
+                    {
+                        "class_name" : null,
+                        "description" : null,
+                        "error_text" : null,
+                        "icon" : "fa-file",
+                        "input_color" : null,
+                        "label_color" : null,
+                        "label" : "File Upload",
+                        "name" : "file-1580318183310",
+                        "placeholder" : null,
+                        "regex" : null,
+                        "subtype" : "file",
+                        "min" : 0,
+                        "max" : 0,
+                        "required" : false,
+                        "toggle" : false,
+                        "_id" : "5e31bdf0022f55171c29e53c",
+                        "type" : "file"
+                    }
+                ]
+            }
+        ],
+        "template_id" : "5e31bdf0022f55171c29e535",
+        "status" : 3,
+        "creation_datetime" : "2020-01-29 15:58:32"
+    },
+    "creation_datetime" : "2020-01-31 14:21:08",
+    "updation_datetime" : "2020-01-31 14:21:18",
+    "status" : 3
+}
